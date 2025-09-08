87874c49bf3fb5e4778d19bda2a2b1e14d1dc81b
danilo <daniloti.zagato@gmail.com>
2025-09-07 23:56:09 -0300

    Adicionando classes para cadastro de endereço

---
+++ src/main/java/com/fiap/techchallenge/restaurantmanagement/core/domain/Cidade.java
@@ -0,0 +1,14 @@
+package com.fiap.techchallenge.restaurantmanagement.core.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cidade {
+    private Long id;
+}
