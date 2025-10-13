6140aff0c52a4e7ac9b84150ed4be01b3b9caaae
Humberto Fernandes <humbertofernandes08@hotmail.com>
2025-10-13 12:40:20 -0300

    Adicionando tratamento para estado não encontrado

--- src/main/java/com/fiap/techchallenge/restaurantmanagement/infra/web/handler/ControllerExceptionHandler.java
+++ src/main/java/com/fiap/techchallenge/restaurantmanagement/infra/web/handler/ControllerExceptionHandler.java
@@ -27,6 +27,17 @@ public class ControllerExceptionHandler {
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(EstadoNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<ErrorResponse> handleEstadoNotFoundException(EstadoNotFoundException ex) {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .status(HttpStatus.NOT_FOUND.value())
+                .message(ex.getMessage())
+                .timestamp(LocalDateTime.now())
+                .build();
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
